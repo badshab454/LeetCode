ba9467e5f23423099d9ccf1ab7964cb0f430e70b
Badsha Hossain <badshab454@gmail.com>
2022-12-26 18:04:38 +0600

    55 jump game js

--- 0055 Jump Game.js
+++ 0055 Jump Game.js
@@ -3,5 +3,14 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    
+    // keep track of max distance traveled so far
+    let max = 0;
+    for(let i=0; i<nums.length; i++){
+        // the only time that max < i is when we are at 0 and we cannot move forward
+        if(i>max) return false;
+        // move the max pointer the maximum
+        max = Math.max(nums[i]+i,max);
+    }
+    // if we did not get stuck at a 0 then return true
+    return true;
 };
