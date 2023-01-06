0aff1c9af36dc19c6f3d305e000b2e0eb811f120
Badsha Hossain <badshab454@gmail.com>
2023-01-06 13:56:10 +0600

    1833 maximum ice cream bars js

--- 1833 Maximum Ice Cream Bars.js
+++ 1833 Maximum Ice Cream Bars.js
@@ -4,5 +4,14 @@
  * @return {number}
  */
 var maxIceCream = function(costs, coins) {
-    
+    let total = 0;
+    let count = 0;
+    costs.sort((a,b)=>a-b)
+    for(let i of costs){
+        total+= i;
+        if(total>coins) break;
+        count++
+
+    }  
+    return count
 };
