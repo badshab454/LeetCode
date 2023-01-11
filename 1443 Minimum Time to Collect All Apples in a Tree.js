7a455d434bf3224623eafe7318b4bbc634b4d6b3
Badsha Hossain <badshab454@gmail.com>
2023-01-11 23:58:55 +0600

    1443 minimum time to collect alll apples in a tree js

--- 1443 Minimum Time to Collect All Apples in a Tree.js
+++ 1443 Minimum Time to Collect All Apples in a Tree.js
@@ -5,5 +5,23 @@
  * @return {number}
  */
 var minTime = function(n, edges, hasApple) {
-    
+    let map = {}
+
+    for(let [x,y] of edges){
+        if(!map[y]) map[y] = []
+        map[y].push(x)
+    }
+    let check = {}
+    let res =0;
+    for(let i =1; i<hasApple.length;i++){
+        if(hasApple[i]){
+            let curV = i;
+            while(curV !=0 && !check[curV]){
+                res +=2;
+                check[curV] = 1;
+                curV = map[curV]
+            }
+        }
+    }
+    return res;
 };
