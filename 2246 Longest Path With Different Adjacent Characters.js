8f66f9b3c0c552275d7c3f862fe9fe9985d8c212
Badsha Hossain <badshab454@gmail.com>
2023-01-13 23:11:22 +0600

    2246 longest path with different adjacent characters js

--- 2246 Longest Path With Different Adjacent Characters.js
+++ 2246 Longest Path With Different Adjacent Characters.js
@@ -4,5 +4,37 @@
  * @return {number}
  */
 var longestPath = function(parent, s) {
-    
+    let result=0;
+    const arr=Array.from({length:s.length},()=>[]);
+    const cache=Array.from({length:s.length},()=>[])
+    for(let i=1;i<s.length;i++){
+      arr[i].push(parent[i])
+      arr[parent[i]].push(i)
+     }
+// array for ex1 i-node can achieve nodes 0->1 or 0->2 ...
+// [ [ 1, 2 ], [ 0, 3, 4 ], [ 0, 5 ], [ 1 ], [ 1 ], [ 2 ] ]
+const dfc=(cur, prev)=>{
+    // if it isn't exist children return 1 if letter is different
+   if(arr[cur].length === 1 && prev!==-1){
+       return +(s[cur]!==s[prev])
+   }
+    for(let node of arr[cur]){
+        // exit if prev node === current
+        if(node===prev) continue;
+        const data=dfc(node, cur);
+        //collect all lengths of children
+        cache[cur].push(data)
+      }
+      // sort to detect 2 longest chains 
+       cache[cur].sort((a,b)=>b-a);
+     // 2 longest chains+1 is a result
+     result=Math.max(result,(cache[cur][0]||0)+(cache[cur][1]||0)+1 )
+      // return max+1 if letter is different and 0 if not
+   return s[cur]!==s[prev]?cache[cur][0]+1:0
+
+
+}
+dfc(0, -1)
+
+return result
 };
