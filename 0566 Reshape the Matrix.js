44af612468d8e20d4807f76f04a2ea3f7293d7e1
Badsha Hossain <badshab454@gmail.com>
2023-01-16 01:21:45 +0600

    566 reshape the matrix js

--- 0566 Reshape the Matrix.js
+++ 0566 Reshape the Matrix.js
@@ -5,5 +5,7 @@
  * @return {number[][]}
  */
 var matrixReshape = function(mat, r, c) {
-    
+    const flat = mat.flat()
+    if (flat.length !== r*c) return mat;
+    return [...Array(r)].map(() => flat.splice(0,c)) 
 };
