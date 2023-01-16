4f99f4dabeb87cf5f2eb42a93e010bc6814f75b1
Badsha Hossain <badshab454@gmail.com>
2023-01-17 00:46:03 +0600

    387 first unique character in a string js solution

--- 0387 First Unique Character in a String.js
+++ 0387 First Unique Character in a String.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    
+    for(let i=0;i<s.length;i++){
+         let ch=s[i]
+         if(s.indexOf(ch)==i && s.indexOf(ch,i+1)==-1){
+                 return i
+         }
+    }
+        return -1
 };
