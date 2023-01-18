24bec5bac1280cd2684f4c80dc722dc3ca5b61e0
Badsha Hossain <badshab454@gmail.com>
2023-01-19 02:03:23 +0600

    203 remove linked list elements js

--- 0203 Remove Linked List Elements.js
+++ 0203 Remove Linked List Elements.js
@@ -10,3 +10,19 @@
  * @param {number} val
  * @return {ListNode}
  */
+var removeElements = function(head, val) {
+    let dummy = new ListNode();
+    let cur = dummy;
+    
+    while (head !== null) {
+        if (head.val !== val) {
+            cur.next = head;
+            cur = cur.next;
+        }
+        head = head.next;
+    }
+    cur.next = null;
+    return dummy.next;
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+};
