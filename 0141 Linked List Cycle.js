86c9f138570a4fa6bfa575a13fcb0f6f180ed0b5
Badsha Hossain <badshab454@gmail.com>
2023-01-19 00:23:54 +0600

    141 linked list cycle js

--- 0141 Linked List Cycle.js
+++ 0141 Linked List Cycle.js
@@ -10,6 +10,14 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    
+
+const hasCycle = (head) => {
+    let fast = head;
+    while (fast && fast.next) {
+        head = head.next;
+        fast = fast.next.next;
+        if (head === fast)
+        return true;
+    }
+    return false;
 };
