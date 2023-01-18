5bd7a4d837cc62dadb2266b1a7eb31b95d558c6a
Badsha Hossain <badshab454@gmail.com>
2023-01-19 00:56:14 +0600

    0021 merge two sorted list js

--- 0021 Merge Two Sorted Lists.js
+++ 0021 Merge Two Sorted Lists.js
@@ -10,6 +10,13 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    
+var mergeTwoLists = function(l1, l2) {
+    if(!l1 || !l2) return l1 || l2
+    if(l1.val < l2.val){
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    }
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
+        
 };
