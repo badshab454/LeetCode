48d51834a0040f1e3b4d951206fdb6a588e21c64
Badsha Hossain <badshab454@gmail.com>
2023-01-19 21:11:25 +0600

    974 subarray sums divisible by k

--- 0974 Subarray Sums Divisible by K.js
+++ 0974 Subarray Sums Divisible by K.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 var subarraysDivByK = function(nums, k) {
-    
+    let n = nums.length;
+    let prefixMod = 0;
+    let result = 0;
+    let modGroups = new Array(k);
+    modGroups[0] = 1;
+    for (let num of nums) {
+        prefixMod = (prefixMod + num % k + k) % k;
+        result += modGroups[prefixMod] || 0;
+        modGroups[prefixMod] = (modGroups[prefixMod] || 0) + 1;
+    }
+    return result;
 };
